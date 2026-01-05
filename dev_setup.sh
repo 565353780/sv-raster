ccadceaea45b922baa104371154ab901292c40d1
chLi <565353780@qq.com>
2026-01-05 23:08:41 +0800

    fix setup issue, success train

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,6 +1,11 @@
 cd ..
 git clone --depth 1 https://github.com/rahul-goel/fused-ssim/
 
+pip install ninja
+
+pip3 install torch torchvision \
+  --index-url https://download.pytorch.org/whl/cu124
+
 pip install numpy einops yacs tqdm natsort argparse \
   pillow imageio imageio-ffmpeg scikit-image pycolmap \
   plyfile shapely gpytoolbox lpips pytorch-msssim
@@ -9,3 +14,9 @@ pip install opencv-python==4.8.0.74
 pip install opencv-contrib-python==4.8.0.74
 pip install trimesh==4.0.4
 pip install open3d==0.18.0
+
+cd fused-ssim
+python setup.py install
+
+cd ../sv-raster/cuda
+python setup.py install
---
+++ train.sh
@@ -0,0 +1,11 @@
+DATA_FOLDER=$HOME/chLi/Dataset/GS/haizei_1
+
+python train.py \
+  --eval \
+  --source_path ${DATA_FOLDER}/gs/ \
+  --model_path ${DATA_FOLDER}/svraster/ \
+  --bound_mode camera_median \
+  --lambda_T_inside 0.01 \
+  --lambda_normal_dmean 0.001 \
+  --lambda_ascending 0.01 \
+  --lambda_sparse_depth 0.01
